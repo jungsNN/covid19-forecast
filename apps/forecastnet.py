50cfee81f08b52ae7f93e84b9e83b5cdb3590799
jungsnn <60561405+jungsNN@users.noreply.github.com>
2021-01-04 23:31:04 -0500

    revised model lstm output size

--- apps/forecastnet.py
+++ apps/forecastnet.py
@@ -1,41 +1,36 @@
 
 class ForecastLSTM(nn.Module):
-    def __init__(self, n_features, out_dim, n_steps, n_layers, hid_dims):
-
+    def __init__(self, n_features, out_dim, n_layers, hid_dim, batch_size):
         super(ForecastLSTM, self).__init__()
-        self.n_steps = n_steps
         self.n_features = n_features
-        self.hid_dims = hid_dims
+        self.out_dim = out_dim
+        self.hid_dim = hid_dim
         self.n_layers = n_layers
+        self.batch_size = batch_size
 
-        self.lstm = nn.LSTM(n_features, hid_dims[0], num_layers=n_layers, batch_first=True, dropout=0.5)
-        self.fc1 = nn.Linear(hid_dims[0], hid_dims[1], bias=False)
-        self.fc2 = nn.Linear(hid_dims[1], out_dim)
-
-        self.dropout = nn.Dropout(0.5)
+        self.lstm = nn.LSTM(n_features, hid_dim, num_layers=n_layers, batch_first=True, dropout=0.5)
+        self.fc = nn.Linear(hid_dim, out_dim)
 
     def forward(self, x, h):
         """
         `(seq_len, batch, input_size)` input size
 
         """
-        batch_size = x.size(0)
-        x = x.view(-1, self.n_steps, self.n_features)
+        x = x.view(self.batch_size, -1, self.n_features)
         lstm_out, h = self.lstm(x, h)
-        lstm_out = lstm_out.contiguous().view(-1, self.n_steps, self.hid_dims[0])[:, -1] # output size (batch*n_steps, hid_dim)
-        out = F.relu(self.fc1(lstm_out))
-        out = self.dropout(out)
-        out = self.fc2(out)
+        out = lstm_out.contiguous().view(-1, self.hid_dim)
+        out = self.fc(out)
+        out = out.view(self.batch_size, -1, self.out_dim)[:, -1]
 
         return out, h
 
-    def init_hidden(self, batch_size):
+    def init_hidden(self):
         weight = next(self.parameters()).data
 
         if torch.cuda.is_available():
-            hidden = (weight.new(self.n_layers, batch_size, self.hid_dim).zero_().cuda(),
-                    weight.new(self.n_layers, batch_size, self.hid_dim).zero_().cuda())
+            hidden = (weight.new(self.n_layers, self.batch_size, self.hid_dim).zero_().cuda(),
+                    weight.new(self.n_layers, self.batch_size, self.hid_dim).zero_().cuda())
         else:
-            hidden = (weight.new(self.n_layers, batch_size, self.hid_dims[0]).zero_(),
-                    weight.new(self.n_layers, batch_size, self.hid_dims[0]).zero_())
+            hidden = (weight.new(self.n_layers, self.batch_size, self.hid_dim).zero_(),
+                    weight.new(self.n_layers, self.batch_size, self.hid_dim).zero_())
         return hidden
